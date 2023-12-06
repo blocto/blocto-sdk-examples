a142d9139f29a6863137caca20fe151bd93673b0
sanyu1225 <pan831225@gmail.com>
2023-12-06 18:44:13 +0800

    feat: web3js v4

---
+++ with-evm-blocto-batch-transaction-v4/src/App.js
@@ -0,0 +1,128 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import { bloctoSDK, web3, contract } from "./services/ethereum";
+import BLTButton from "./components/Button";
+
+export default function App() {
+  const [address, setAddress] = useState(null);
+  const [txHash, setTxHash] = useState(null);
+
+  const loginHandler = async () => {
+    const accounts = await bloctoSDK?.ethereum?.request({
+      method: "eth_requestAccounts"
+    });
+    setAddress(accounts[0]);
+  };
+
+  const sendBatchTransactionHandler = async () => {
+    try {
+      const recipient = "0xc61B4Aa62E5FD40cceB08C602Eb5D157b257b49a"; // replace with the recipient's address
+      const recipient2 = "0x0000000000000000000000000000000000000000"; // replace with the recipient's address
+      const batch = new web3.BatchRequest();
+
+      const request0 = {
+        jsonrpc: '2.0',
+        id: 11,
+        method:"eth_sendTransaction",
+        params:[{
+          from: address,
+          to: recipient,
+          data: contract.methods.setValue(123).encodeABI()
+         }]
+      }
+      const request1 = {
+        jsonrpc: '2.0',
+        id: 10,
+        method: 'eth_getBalance',
+        params: ['0xf4ffff492596ac13fee6126846350433bf9a5021', 'latest'],
+      };
+      const request2 = {
+        jsonrpc: '2.0',
+        id: 12,
+        method: 'eth_getBalance',
+        params: ['0xdc6bad79dab7ea733098f66f6c6f9dd008da3258', 'latest'],
+      };
+      const request3 = {
+        jsonrpc: '2.0',
+        id: 16,
+        method:"eth_sendTransaction",
+        params:[{
+          from: address,
+          to: recipient2,
+          data: contract.methods.setValue(123).encodeABI()
+         }]
+      }
+      batch.add(request0);
+      batch.add(request1);
+      // batch.add(request2);
+      // batch.add(request3);
+
+      const responses = await batch.execute({
+        timeout: 500000,
+      });
+      console.log('responses: ', responses);
+
+      // const _txHash = await bloctoSDK.ethereum.request({
+      //   method: "blocto_sendBatchTransaction",
+      //   params: [
+      //     ...web3.eth.sendTransaction.request({
+      //       from: address,
+      //       to: recipient,
+      //       data: contract.methods.setValue(123).encodeABI()
+      //     }).params,
+      //     ...web3.eth.sendTransaction.request({
+      //       from: address,
+      //       to: recipient,
+      //       data: contract.methods.setValue(123).encodeABI()
+      //     }).params
+      //   ]
+      // });
+
+      // setTxHash(_txHash);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const sendBatchWithRPC = async () => {
+    try {
+      const recipient = "0xc61B4Aa62E5FD40cceB08C602Eb5D157b257b49a"; // replace with the recipient's address
+      const _txHash = await bloctoSDK.ethereum.request({
+        method: "wallet_sendMultiCallTransaction",
+        params: [
+          [
+            {
+              from: address,
+              to: recipient,
+              data: contract.methods.setValue(123).encodeABI()
+            },
+            {
+              from: address,
+              to: recipient,
+              data: contract.methods.setValue(123).encodeABI()
+            }
+          ],
+          true
+        ]
+      });
+      console.log('_txHash',_txHash);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      {address ? (
+        <>
+          <BLTButton onClick={sendBatchTransactionHandler}>Send</BLTButton>
+          <p>txHash: {txHash}</p>
+          <BLTButton onClick={sendBatchWithRPC}>With RPC</BLTButton>
+        </>
+      ) : (
+        <BLTButton onClick={loginHandler}>Login</BLTButton>
+      )}
+    </div>
+  );
+}
