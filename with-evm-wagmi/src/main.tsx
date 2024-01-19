7360d40cee18ae248f0ae530e8b28ba01c606ee3
Calvin Chang <q20274982@gmail.com>
2024-01-19 11:53:06 +0800

    feat: add wagmi v2 example

---
+++ with-evm-wagmi/src/main.tsx
@@ -0,0 +1,45 @@
+import { Buffer } from 'buffer'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WagmiProvider, deserialize, serialize } from 'wagmi'
+
+// `@coinbase-wallet/sdk` uses `Buffer`
+globalThis.Buffer = Buffer
+
+import App from './App.tsx'
+import { config } from './wagmi.ts'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1_000 * 60 * 60 * 24, // 24 hours
+      networkMode: 'offlineFirst',
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+    mutations: { networkMode: 'offlineFirst' },
+  },
+})
+
+const persister = createSyncStoragePersister({
+  key: 'vite-react.cache',
+  serialize,
+  storage: window.localStorage,
+  deserialize,
+})
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
+        <App />
+      </PersistQueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
+)
