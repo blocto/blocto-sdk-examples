45e7eb13ca3f1ee2788390f31c5d3484148b409f
sanyu1225 <pan831225@gmail.com>
2024-03-21 20:57:06 +0800

    feat: github action config

--- updateAllExamples.mjs
+++ updateAllExamples.mjs
@@ -4,11 +4,11 @@ import { getDirectories, updatePackageJSON } from "./utils.js";
   try {
     const evmDirectories = await getDirectories("./src/evm");
     const aptosDirectories = await getDirectories("./src/aptos");
-    const getNpmSdkVersion = await fetch(
-      `https://registry.npmjs.org/@blocto/sdk/latest`
-    )
-      .then((res) => res.json())
-      .then(({ version }) => version);
+    const getNpmSdkVersion = async () => {
+      return fetch("https://registry.npmjs.org/@blocto/sdk/latest")
+        .then((res) => res.json())
+        .then(({ version }) => version);
+    };
     // process.argv[2] from shell script verison like `yarn updatAll x.x.x`
     const version = process.argv[2]
       ? process.argv[2]
